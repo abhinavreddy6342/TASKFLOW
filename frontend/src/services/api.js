7e72b71a23a8d6ac1da85037a006d8fc225d12b0
abhinavreddy6342 <abhinavreddy054@gmail.com>
2026-08-23 11:11:18 +0530

    Connect frontend to production backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://taskflow-backend-sw7t.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
